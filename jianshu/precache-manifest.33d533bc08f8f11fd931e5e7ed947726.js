a8d04ac8b30dcf2735a48ae65256fb5bb92beb89
Sophil-minn <1056640663@qq.com>
2019-12-07 21:16:34 +0800

    我的简书项目

---
+++ jianshu/precache-manifest.33d533bc08f8f11fd931e5e7ed947726.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b7f3528d6ef9beeded867cf4734228f5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "cb85535609829e7cc631",
+    "url": "/static/js/2.9bd8f279.chunk.js"
+  },
+  {
+    "revision": "be1a79632a987511058c",
+    "url": "/static/js/3.31b27521.chunk.js"
+  },
+  {
+    "revision": "1a4ae0241708cf86cd90",
+    "url": "/static/js/main.88a4cb86.chunk.js"
+  },
+  {
+    "revision": "4db3adfda7e1db280d79",
+    "url": "/static/js/runtime-main.51f01902.js"
+  }
+]);
